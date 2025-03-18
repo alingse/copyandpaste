36fef23a4dba3b055f30c4a3b328090122da3d99
alingse <alingse@foxmail.com>
2025-03-18 23:10:30 +0800

    add some repeat args funcs

--- testdata/src/repeatargs/positive.go
+++ testdata/src/repeatargs/positive.go
@@ -1,24 +1,35 @@
 package repeatargs
 
 import (
+	"bytes"
+	"cmp"
 	"maps"
 	"math"
+	"os"
+	"reflect"
 	"slices"
 	"strings"
 )
 
-func Do(a float64) {
-	_ = math.Max(a, a) // want `the args should be different`
-	_ = math.Min(a, a) // want `the args should be different`
+func Do() {
+	a := 1.0
+	bs := []byte{}
+	m := map[int]int{1: 1}
+	s := ""
 
-	_ = math.Min(a+a, a+a) // want `the args should be different`
-
-	var s = []float64{a}
-	_ = slices.Equal(s, s) // want `the args should be different`
-
-	var m = map[int]int{1: 1}
-	maps.Equal(m, m) // want `the args should be different`
-
-	var s1 string = "a"
-	strings.EqualFold(s1, s1) // want `the args should be different`
+	_ = bytes.Compare(bs, bs)   // want `the args should be different`
+	_ = bytes.Equal(bs, bs)     // want `the args should be different`
+	_ = bytes.Index(bs, bs)     // want `the args should be different`
+	_ = cmp.Compare(s, s)       // want `the args should be different`
+	_ = maps.Equal(m, m)        // want `the args should be different`
+	_ = math.Dim(a, a)          // want `the args should be different`
+	_ = math.Min(a, a)          // want `the args should be different`
+	_ = math.Max(a, a)          // want `the args should be different`
+	_ = os.Rename(s, s)         // want `the args should be different`
+	_ = reflect.DeepEqual(m, m) // want `the args should be different`
+	_ = slices.Compare(bs, bs)  // want `the args should be different`
+	_ = slices.Equal(bs, bs)    // want `the args should be different`
+	_ = strings.Compare(s, s)   // want `the args should be different`
+	_ = strings.EqualFold(s, s) // want `the args should be different`
+	_ = strings.Index(s, s)     // want `the args should be different`
 }
